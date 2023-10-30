cd0bf33149f42bf9f8f216acd2cedb801294b318
tih4r3-chan <ticampuoli18@gmail.com>
2023-10-30 14:58:55 -0300

    new
    aaaaaaa

--- centroMedico/models.py
+++ centroMedico/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+# crear clase del proyecto 
+class Users(models.Model):
+    nombre = models.CharField(max_length=100, default='DEFAULT VALUE')
+    apellido = models.CharField(max_length=100, default='DEFAULT VALUE')
+    email = models.CharField(max_length=100, default='DEFAULT VALUE')
+    tipoUsuario = models.CharField(max_length=60, default='DEFAULT VALUE')
+    password = models.CharField(max_length=100, default='DEFAULT VALUE')
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.nombre
+
+
+class tipoUsuario(models.Model):
+    tipo = models.CharField(max_length=100, default='DEFAULT VALUE')
+
+    class Meta:
+        db_table = 'tipoUsuario'
+
+    def __str__(self):
+        return self.tipo
